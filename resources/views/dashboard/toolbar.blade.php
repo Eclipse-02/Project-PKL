9fc982b5457f1ab6f007b23e487a98e6d4e9766c
Ecliße <rafaumar22@gmail.com>
2025-02-01 14:05:11 +0700

    new component toolbar

---
+++ resources/views/dashboard/toolbar.blade.php
@@ -0,0 +1,36 @@
+<!--begin::Row-->
+<div class="row justify-content-center">
+    <!--begin::Col-->
+    <div class="col-auto">
+        <!--begin::Widget-->
+        <div class="card shadow-sm my-3">
+            <!--begin::Body-->
+            <div class="card-body d-flex flex-row px-6 py-3">
+                <!--begin::Button-->
+                <button id="search" class="btn btn-outline btn-outline-dashed btn-outline-primary btn-active-light-primary p-3 mx-2" data-bs-toggle="tooltip" data-bs-delay-show="500" data-bs-custom-class="tooltip-dark" data-bs-placement="bottom" title="Search" data-bs-dimiss="click">
+                    <i class="bi bi-search fs-2 p-0"></i>
+                </button>
+                <!--end::Button-->
+                <!--begin::Button-->
+                <button id="add" class="btn btn-outline btn-outline-dashed btn-outline-success btn-active-light-success p-3 mx-2" data-bs-toggle="tooltip" data-bs-delay-show="500" data-bs-custom-class="tooltip-dark" data-bs-placement="bottom" title="Add" data-bs-dimiss="click">
+                    <i class="bi bi-file-earmark-plus fs-2 p-0"></i>
+                </button>
+                <!--end::Button-->
+                <!--begin::Button-->
+                <button id="save" class="btn btn-outline btn-outline-dashed btn-outline-warning btn-active-light-warning p-3 mx-2" data-bs-toggle="tooltip" data-bs-delay-show="500" data-bs-custom-class="tooltip-dark" data-bs-placement="bottom" title="Save" data-bs-dimiss="click">
+                    <i class="bi bi-floppy fs-2 p-0"></i>
+                </button>
+                <!--end::Button-->
+                <!--begin::Button-->
+                <button id="delete" class="btn btn-outline btn-outline-dashed btn-outline-danger btn-active-light-danger p-3 mx-2" data-bs-toggle="tooltip" data-bs-delay-show="500" data-bs-custom-class="tooltip-dark" data-bs-placement="bottom" title="Delete" data-bs-dimiss="click">
+                    <i class="bi bi-file-earmark-minus fs-2 p-0"></i>
+                </button>
+                <!--end::Button-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Widget-->
+    </div>
+    <!--end::Col-->
+</div>
+<!--end::Row-->
